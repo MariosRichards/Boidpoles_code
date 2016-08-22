0be73289caea989fbe2a36482d498c851e9f45c7
Xantomen <xantomen@gmail.com>
2016-08-22 18:24:46 +0200

    Testing Destroy on Click

--- src/js/objects_declaration_and_render.js
+++ src/js/objects_declaration_and_render.js
@@ -1,15 +1,14 @@
 var svg = d3.select("body").append("svg")
     .attr("width", CANVAS_WIDTH)
     .attr("height", CANVAS_HEIGHT);
+    
 
-var g = svg.selectAll("g")
-    .data(spermatozoa)
-  .enter().append("g");
+var g = updatePoles();
 
 var head = g.append("ellipse")
-    .attr("rx", 6.5)
-    .attr("ry", 4);
-
+	    .attr("rx", 6.5)
+	    .attr("ry", 4);
+	
 g.append("path")
     .datum(function(d) { return d.path.slice(0, 3); })
     .attr("class", "mid");
@@ -19,3 +18,27 @@ g.append("path")
     .attr("class", "tail");
 
 var tail = g.selectAll("path");
+
+function updatePoles()
+{
+	
+	var group = svg.selectAll("g")
+    .data(spermatozoa, function(d){ return d.key; });
+    
+    group.enter().append("g").on("click", function(d, i){
+   	   	   	   	
+   		spermatozoa.splice(i, 1);
+   		   		
+   		NUMBER_POLES -= 1;
+   	
+    	updatePoles();
+    
+	 });
+	 
+	 group.exit().remove();
+
+	 return group;
+	
+	
+}
+
