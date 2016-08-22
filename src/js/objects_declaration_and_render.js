8d52f1a5634aab7d3c52733bc8a04a693c62c12f
Xantomen <xantomen@gmail.com>
2016-08-22 22:08:57 +0200

    Implemented pole_population_control functions

--- src/js/objects_declaration_and_render.js
+++ src/js/objects_declaration_and_render.js
@@ -2,46 +2,26 @@ var svg = d3.select("body").append("svg")
     .attr("width", CANVAS_WIDTH)
     .attr("height", CANVAS_HEIGHT);
     
+var head;
+var tail;
 
-var g = updatePoles();
-
-var head = g.append("ellipse")
-	    .attr("rx", 6.5)
-	    .attr("ry", 4);
-	
-g.append("path")
-    .datum(function(d) { return d.path.slice(0, 3); })
-    .attr("class", "mid");
-
-g.append("path")
-    .datum(function(d) { return d.path; })
-    .attr("class", "tail");
-
-var tail = g.selectAll("path");
+updatePoles();
 
 function updatePoles()
 {
 	
 	var group = svg.selectAll("g")
     .data(spermatozoa, function(d){ return d.key; });
-    
-    /*group.enter().append("g").on("click", function(d, i){
-   	   	   	   	
-   		spermatozoa.splice(i, 1);
-   		   		
-   		NUMBER_POLES -= 1;
-   	
-    	updatePoles();
-    
-	 });*/
-	
+    	
 	 var groupNew = group.enter().append("g");
 	 
 	 group.exit().remove();
 	 
-	 var head = groupNew.append("ellipse")
+	 head_new = groupNew.append("ellipse")
 	    .attr("rx", 6.5)
 	    .attr("ry", 4);
+	    
+	 head = group.selectAll("ellipse");  
 	
 	groupNew.append("path")
 	    .datum(function(d) { return d.path.slice(0, 3); })
@@ -51,55 +31,10 @@ function updatePoles()
 	    .datum(function(d) { return d.path; })
 	    .attr("class", "tail");
 	
-	var tail = groupNew.selectAll("path");
+	tail = group.selectAll("path");
 	 
 	return group;
 
 }
 
-function deletePoleByKey(key)
-{
-	spermatozoa.splice(key, 1);
-	
-	updatePoles();
-}
-
-function createNewPole()
-{	
-	
-	UNIQUE_KEY += 1;
-	
-	var x = Math.random() * CANVAS_WIDTH,
-		y = Math.random() * CANVAS_HEIGHT;
-	
-	var new_pole = {
-		
-		vx: getVelocityValueX(),
-	    vy: getVelocityValueY(),
-	    path: d3.range(TAIL_LENGTH).map(function() { return [x, y]; }),
-	    count: 0,
-	    key: UNIQUE_KEY
-		
-	};
-	
-	spermatozoa.push(new_pole);
-					
-	updatePoles();
-		
-}
-
-function deleteAllPoles()
-{
-	spermatozoa = [];
-	
-	updatePoles();
-}
-
-function createManyPoles(number) {
-	
-	for(var i=0;i<number;i++)
-	{
-		createNewPole();
-	}
-}
 
---
+++ src/js/pole_population_control.js
@@ -0,0 +1,46 @@
+function deletePoleByKey(key)
+{
+	spermatozoa.splice(key, 1);
+	
+	updatePoles();
+}
+
+function createNewPole()
+{	
+	
+	UNIQUE_KEY += 1;
+	
+	var x = Math.random() * CANVAS_WIDTH,
+		y = Math.random() * CANVAS_HEIGHT;
+	
+	var new_pole = {
+		
+		vx: getVelocityValueX(),
+	    vy: getVelocityValueY(),
+	    path: d3.range(TAIL_LENGTH).map(function() { return [x, y]; }),
+	    count: 0,
+	    key: UNIQUE_KEY
+		
+	};
+	
+	spermatozoa.push(new_pole);
+					
+	updatePoles();
+		
+}
+
+function deleteAllPoles()
+{
+	spermatozoa = [];
+	
+	updatePoles();
+
+}
+
+function createManyPoles(number) {
+	
+	for(var i=0;i<number;i++)
+	{
+		createNewPole();
+	}
+}
