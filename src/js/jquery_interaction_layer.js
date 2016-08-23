a607692e606c0da43e96105954812917a17f3c18
Xantomen <xantomen@gmail.com>
2016-08-23 15:26:31 +0200

    Adding some panel Buttons for Pole and Bullet management

--- src/js/jquery_interaction_layer.js
+++ src/js/jquery_interaction_layer.js
@@ -1,10 +1,57 @@
 $(document).ready(function(){
 
-	$("body").click(function(){
+	$("#create_pole").click(function(e){
 		
-		
+		createNewPole();
 		
 	});
+	$("#create_poles").click(function(e){
+		
+		var number = $("#pole_number").val();
+		
+		if(number)
+		{
+			createManyPoles(number);
+		}
+		
+	});
+	$("#pole_number").click(function(e){
+		
+		e.stopPropagation();
+		
+	});
+	$("#destroy_poles").click(function(e){
+		
+		deleteAllPoles();
+		
+	});
+	
+	$("#create_bullet").click(function(e){
+		
+		createNewBullet(0,0);
+		
+	});
+	$("#create_bullets").click(function(e){
+		
+		var number = $("#bullet_number").val();
+		
+		if(number)
+		{
+			createManyBullets(number);
+		}
+		
+	});
+	$("#bullet_number").click(function(e){
+		
+		e.stopPropagation();
+		
+	});
+	$("#destroy_bullets").click(function(e){
+		
+		deleteAllBullets();
+		
+	});
+	
 	
 	
 });
